d1f51549a758897de7c9cdf2bfbd98306a78fd4e
Sergey <tchbreadman@gmail.com>
2025-01-15 15:23:35 +0700

    add description for entity User

--- src/main/java/ru/safonov/trainingmodule/BackEnd/models/User.java
+++ src/main/java/ru/safonov/trainingmodule/BackEnd/models/User.java
@@ -8,6 +8,8 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+
+//Entity User
 @Entity
 @Table(name = "Users")
 public class User {
